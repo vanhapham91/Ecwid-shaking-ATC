9290a3670da8c692fe69325de82c5febdf3a48a6
Hannah <vanhapham91@gmail.com>
2020-06-17 16:16:56 +0700

    SDK finish

--- sdk/sdk.js
+++ sdk/sdk.js
@@ -1,35 +1,43 @@
 // Get app public config value from Ecwid
-// let shakingAddToCartConfig = Ecwid.getAppPublicConfig('shaking-add-to-cart');
-// shakingAddToCartConfig = JSON.parse(shakingAddToCartConfig);
+let shakingAddToCartConfig = Ecwid.getAppPublicConfig('add-to-cart-shake-animator-dev');
+shakingAddToCartConfig = JSON.parse(shakingAddToCartConfig);
 
-// FAKE DATA
-let shakingAddToCartConfig = {
-    enabled: true,
-    animation: 'shake-lr', // animation list: vibrate, shake-horizontal, shake-vertical, shake-lr
-    delayTime: null, // for type Automatic only  - between each animation
-    type: 'hover' // or hover
-};
-
-const {
-    enabled,
-    animation,
-    delayTime,
-    type,
-} = shakingAddToCartConfig
-
-// If the app is enabled in storefront
-if(enabled) {
-    // Get all add to cart buttons on screen and add animated class.
-    const addToCartBtns = $('.grid-product__buy-now button');
-    let animateClass = `oe-animate-${animation}`;
-    if (type === 'automatic') {
-        animateClass = animateClass.concat('--auto');
-    }
-    addToCartBtns.addClass(`oe-animated-btn ${animateClass}`);
-
-    // Change animation delay time (automatic type only)
-    if (delayTime) {
-        addToCartBtns.css('animation-delay', `${delayTime}s`);
-    }
+if (!shakingAddToCartConfig) {
+    shakingAddToCartConfig = {
+        enabled: 'false',
+        animation: 'vibrate', // animation list: vibrate, shake-horizontal, shake-vertical, shake-lr
+        delayTime: null, // for type Automatic only  - between each animation
+        type: 'true' // or hover
+    };
 }
 
+const {
+    isEnabled,
+    animation,
+    delayTime,
+    triggerType,
+} = shakingAddToCartConfig;
+
+// If the app is enabled in storefront
+window.onload = function() {
+    if(isEnabled === 'true') {
+        // Get all add to cart buttons on screen and add animated class.
+        const findButtom = setInterval(function() {
+            const addToCartBtns = $('.details-product-purchase__add-to-bag button');
+            if ( addToCartBtns.length > 0 ) {
+                let animateClass = `satc-animate-${animation}`;
+                if (triggerType === 'auto') {
+                    animateClass = animateClass.concat('--auto');
+                }
+                addToCartBtns.addClass(`satc-animated-btn ${animateClass}`);
+                // Change animation delay time (automatic type only)
+                if (triggerType === 'hover') {
+                    addToCartBtns.css('animation-delay', 0);
+                } else {
+                    addToCartBtns.css('animation-delay', `${delayTime}s`);
+                }
+               clearInterval(findButtom);
+           }
+        }, 200);
+    }
+};
